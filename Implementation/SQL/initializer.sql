85d0ee5cafc51631c7a7f76cd96153f421b3d4f9
Dylan H <Dylanhism@users.noreply.github.com>
2021-12-19 01:37:43 -0500

    Format initializer.sql and update to new column names

--- Implementation/SQL/initializer.sql
+++ Implementation/SQL/initializer.sql
@@ -1,72 +1,63 @@
-DROP DATABASE (bookstore);
-
-CREATE DATABASE (bookstore);
-
-CREATE TABLE Book (
-    ISBN char(13) NOT NULL,
-    genre varchar(15) NOT NULL,
-    price decimal(3, 2) NOT NULL,
-    name varchar(50) NOT NULL,
-    num_pages int NOT NULL,
-    quantity int NOT NULL DEFAULT 0,
-    publisher_name NOT NULL,
-    PRIMARY KEY (ISBN),
-    FOREIGN KEY (publisher_name) REFERENCES Publisher(publisher_name)
-);
-
+/*This file drops if already present and reconstructs the whole database with tables*/
+DROP DATABASE bookstore;
+CREATE DATABASE bookstore;
 CREATE TABLE Publisher (
-    publisher_name varchar(30) NOT NULL,
-    email_address varchar(30) NOT NULL,
+    publisher_name varchar(50) NOT NULL,
+    email_address varchar(50) NOT NULL,
     address varchar(50) NOT NULL,
-    phone_num int NOT NULL,
+    phone_num bigint NOT NULL,
     bank_account varchar(30) NOT NULL,
     PRIMARY KEY (publisher_name)
 );
-
+CREATE TABLE Book (
+    ISBN char(13) NOT NULL,
+    genre varchar(25) NOT NULL,
+    price decimal(4, 2) NOT NULL,
+    name varchar(50) NOT NULL,
+    num_pages int NOT NULL,
+    quantity int NOT NULL DEFAULT 0,
+    publisher_name varchar(50) NOT NULL,
+    PRIMARY KEY (ISBN),
+    FOREIGN KEY (publisher_name) REFERENCES Publisher(publisher_name)
+);
 CREATE TABLE Author (
     author_ID int NOT NULL AUTO_INCREMENT,
-    name varchar(30),
+    author_name varchar(30),
     PRIMARY KEY (author_ID)
 );
-
-CREATE TABLE Written_by (
+CREATE TABLE Written_by ( /*Connecting table for book and author*/
     ISBN char(13) NOT NULL,
     author_ID int NOT NULL,
     PRIMARY KEY (ISBN, author_ID),
-    FOREIGN KEY Book(ISBN),
-    FOREIGN KEY Author(author_ID)
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN),
+    FOREIGN KEY (author_ID) REFERENCES Author(author_ID)
 );
-
 CREATE TABLE Owner (
-    username varchar(15) NOT NULL,
-    password varchar(15) NOT NULL,
+    username varchar(25) NOT NULL,
+    password varchar(25) NOT NULL,
     PRIMARY KEY (username)
 );
-
 CREATE TABLE User (
-    username varchar(15) NOT NULL,
-    password varchar(15) NOT NULL,
-    name varchar(30) NOT NULL,
-    email varchar(30) NOT NULL UNIQUE,
-    phone_num int,
+    username varchar(25) NOT NULL,
+    password varchar(25) NOT NULL,
+    user_name varchar(30) NOT NULL,
+    email varchar(50) NOT NULL UNIQUE,
+    phone_num bigint,
     PRIMARY KEY (username)
 );
-
-CREATE TABLE Order (
+CREATE TABLE Orders (
     order_ID int NOT NULL AUTO_INCREMENT,
-    status varchar(15) NOT NULL,
-    location varchar(30) NOT NULL,
+    order_status varchar(15) NOT NULL,
+    order_location varchar(30) NOT NULL,
+    shipping_addr varchar(50) NOT NULL,
     username varchar(15) NOT NULL,
     PRIMARY KEY (order_ID),
-    FOREIGN KEY User(username)
+    FOREIGN KEY (username) REFERENCES User(username)
 );
-
-CREATE TABLE Order_contains (
+CREATE TABLE Order_contains ( /*Connecting table for order and book*/
     order_ID int NOT NULL,
     ISBN char(13) NOT NULL,
     PRIMARY KEY (order_ID, ISBN),
-    FOREIGN KEY Order(order_ID),
-    FOREIGN KEY Book(ISBN)
+    FOREIGN KEY (order_ID) REFERENCES Orders(order_ID),
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN)
 );
-
-
